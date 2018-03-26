3db328a161da561638a2582e95b1a9f998d956ff
ntkduy123 <ntkduy123@gmail.com>
2018-03-26 23:18:36 +0700

    first commit

---
+++ src/main/java/com/duynguyen/personal/personalproject/PersonalProjectApplication.java
@@ -0,0 +1,14 @@
+package com.duynguyen.personal.personalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class PersonalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalProjectApplication.class, args);
+	}
+}
---
+++ src/main/java/com/duynguyen/personal/personalproject/domain/Article.java
@@ -0,0 +1,91 @@
+package com.duynguyen.personal.personalproject.domain;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Article
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+
+    private String category;
+
+    private String description;
+
+    private Date date;
+
+    private String summary;
+
+    private String reference;
+
+    @Lob
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+}
