ce948208483b73461d916b4d1d4096927f37e5fd
ntkduy123 <ntkduy123@gmail.com>
2018-03-26 23:44:49 +0700

    add angular front end

--- src/main/java/com/duynguyen/personal/personalproject/PersonalProjectApplication.java
+++ src/main/java/com/duynguyen/personal/personalproject/PersonalProjectApplication.java
@@ -3,6 +3,10 @@ package com.duynguyen.personal.personalproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -11,4 +15,16 @@ public class PersonalProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PersonalProjectApplication.class, args);
 	}
+
+	@Bean
+	WebMvcConfigurer configurer () {
+		return new WebMvcConfigurerAdapter() {
+
+			@Override
+			public void addResourceHandlers (ResourceHandlerRegistry registry) {
+				registry.addResourceHandler("/dist/**").addResourceLocations("classpath:/static/dist/");
+			}
+		};
+	}
+
 }
